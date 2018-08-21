6c15b511958ae2866fcabf8f787bfdbd43982b7b
tom <tom.block@brockmann-consult.de>
2018-08-21 16:17:54 +0200

    finished postGIS test version 1

--- tests/core/db/db_spike_postgis.py
+++ tests/core/db/db_spike_postgis.py
@@ -15,6 +15,7 @@ def main(args=None):
 
         initialize_database(connection, cursor)
         fill_database(connection, cursor)
+        search_database(connection, cursor)
 
     finally:
         clean_up_db(connection, cursor)
@@ -30,7 +31,7 @@ def initialize_database(conn, cursor):
     print("done")
 
 def fill_database(conn, cursor):
-    num_entries = 10000
+    num_entries = 1000000
     print("fill database ({} values)...".format(num_entries))
 
     t_start = time.clock()
@@ -42,9 +43,24 @@ def fill_database(conn, cursor):
             lat = 90.0 - (random() * 180.0)
             # point_wkt = 'POINT({0} {1})'.format(lon, lat)
             desc_text = "A random description, version " + str(idx)
-            cursor.execute('''INSERT INTO in_situ_point(location, description) VALUES (ST_Point(%s, %s), %s);''', (lon, lat, desc_text, ))
+            cursor.execute('''INSERT INTO in_situ_point(location, description) VALUES ( ST_SetSRID( ST_Point(%s, %s), 4326), %s);''', (lon, lat, desc_text, ))
     finally:
         conn.commit()
+
+    print("done - ({} sec)".format(time.clock() - t_start))
+
+def search_database(conn, cursor):
+    print("search database...")
+    t_start = time.clock()
+
+    try:
+        cursor.execute("SELECT * FROM in_situ_point WHERE ST_Contains(ST_GeomFromText('POLYGON((-30 -60, -25 -60, -25 -55, -30 -55, -30 -60))', 4326)::geometry, in_situ_point.location::geometry);")
+        #cursor.execute("SELECT ST_AsText(in_situ_point.location) FROM in_situ_point")
+        results = cursor.fetchall()
+        print(len(results))
+    except Exception as e:
+        print(e)
+        conn.rollback()
 
     print("done - ({} sec)".format(time.clock() - t_start))
 
